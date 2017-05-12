5a0e99bc43fd085cdcf718e497ea6ed98d1e8e11
ray soyama <raysoyamaschool@gmail.com>
2017-05-12 14:14:20 -0700

    Added outputs and made the buttons work n shit

--- src/Main.java
+++ src/Main.java
@@ -4,7 +4,7 @@
 import javax.swing.JFrame;
 import javax.swing.JButton;
 
-import java.awt.FlowLayout;
+import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 
@@ -12,27 +12,55 @@ import java.awt.event.ActionListener;
 public class Main extends JFrame implements ActionListener {
 
     public static void main(String[] args) {
+
         new Main().setVisible(true);
     }
 
     private Main(){
         //name of the screen
         super("Start Screen");
+
         //makes the size of the screen width by height
         setSize(600,600);
         setResizable(false);
+
         //When you close the frame, end code
         setDefaultCloseOperation(EXIT_ON_CLOSE);
-        setLayout(new FlowLayout());
+        setLayout(new BorderLayout());
+
+        /*
+        (this is for above)
+         FlowLayout
+         GridBagLayout
+         GridLayout (row,column)
+         BorderLayout ( add(button, BorderLayout.WEST)CENTER)
+         */
 
         //makes a new button
         JButton loadButton = new JButton("Load");
+        JButton CreateButton = new JButton("Create");
+
+        //makes the action listener peramiters this
+        loadButton.setActionCommand("Test");
+        CreateButton.setActionCommand("Teeest");
+
+        //makes the button work
+        loadButton.addActionListener(this);
+        CreateButton.addActionListener(this);
 
         //Creates the button onto the UI
         add(loadButton);
+        add(CreateButton);
     }
        @Override
     public void actionPerformed(ActionEvent actionEvent) {
-           System.out.print("Aye good job");
-    }
+           String name = actionEvent.getActionCommand();
+           //you can set .equals to .equasIgnoreCase if want
+           if (name.equals("Test")) {
+               System.out.println("Good shit");
+
+           } else if (name.equals("Teeest")) {
+               System.out.println("Good job");
+           }
+       }
 }
